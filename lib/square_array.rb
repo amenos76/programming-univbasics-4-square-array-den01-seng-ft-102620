3f4220f56ffb480976baa9590c2f9e571cb8204a
Augie Menos <augie.menos@gmail.com>
2020-10-14 21:03:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_numbers = []
   array.length.times do |index|
-    new_numbers.push(array[index] ** array[index])
+    new_numbers.push(array[index] * array[index])
   end
   new_numbers
 end
